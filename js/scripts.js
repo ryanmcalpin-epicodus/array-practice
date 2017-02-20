7fc7148b9007ce86297f00d5a82da208ffa8a285
Evan Bishop and Ryan McAlpin <evan@>
2017-02-20 13:40:11 -0800

    add sentence enter program

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
+// ICE CREAM
 // // $(document).ready(function() {
 // //   var flavors = ["chocolate", "vanilla", "rocky road", "coconut", "cherry", "plain"];
 // //   flavors.forEach(function(flavor){
 // //     $("#unList").prepend("<li>" + flavor + "</li>");
 // //   });
 // // });
-//
+
+
+ // GROCERY
 // $(document).ready(function(){
 //   $("#grossForm").submit(function(event){
 //     var groceryArray =[$('input#grocery1').val(), $('input#grocery2').val(), $('#grocery3').val(), $('#grocery4').val()]
@@ -23,11 +26,35 @@
 //   })
 // });
 
+// SENTENCE ENTER CAVE OF MYSTERY
 $(document).ready(function(){
   $("#formy").submit(function(event){
-  var firstArray = $("#sentence").val().split();
+    var arrayOfStrings = [];
+    function splitString(stringToSplit, separator) {
+      arrayOfStrings = stringToSplit.split(separator);
+      // console.log('The original string is: "' + stringToSplit + '"');
+      // console.log('The separator is: "' + separator + '"');
+      // console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
+      return arrayOfStrings;
+    }
 
-  alert(firstArray);
-  event.preventDefault();
+    var sentenceInput = $("#sentence").val();
+    var space = ' ';
+
+    console.log(splitString(sentenceInput, space));
+    
+    var newArray = arrayOfStrings.map(function(word){
+      console.log("derp test");
+      if (word.length >= 3){
+        return word;
+      }
+      // console.log(newArray);
+    });
+
+
+    console.log(arrayOfStrings);
+    console.log(newArray);
+
+    event.preventDefault();
   });
 });
