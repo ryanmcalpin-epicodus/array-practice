2ce8794d53e393f17f33a3ba538067a6acc19450
Evan Bishop and Ryan McAlpin <ryan@>
2017-02-20 14:25:11 -0800

    add a forEach inside a forEach and append <li> elements in a <ul> to dispaly every possible suit of card.

--- js/scripts.js
+++ js/scripts.js
@@ -26,24 +26,42 @@
 //   })
 // });
 
-// SENTENCE ENTER CAVE OF MYSTERY
-$(document).ready(function(){
-  $("#formy").submit(function(event){
-    var arrayOfStrings = [];
-    function splitString(stringToSplit, separator) {
-      arrayOfStrings = stringToSplit.split(separator);
-      return arrayOfStrings;
-    }
-    var sentenceInput = $("#sentence").val();
-    // var space = ' ';
-    console.log(splitString(sentenceInput, " "));
+// // SENTENCE ENTER CAVE OF MYSTERY
+// $(document).ready(function(){
+//   $("#formy").submit(function(event){
+//     var arrayOfStrings = [];
+//     function splitString(stringToSplit, separator) {
+//       arrayOfStrings = stringToSplit.split(separator);
+//       return arrayOfStrings;
+//     }
+//     var sentenceInput = $("#sentence").val();
+//     // var space = ' ';
+//     console.log(splitString(sentenceInput, " "));
+//
+//     var newArray = arrayOfStrings.filter(function(word){
+//       return word.length >= 3;
+//     });
+//     console.log(arrayOfStrings);
+//     console.log(arrayOfStrings[1]);
+//     console.log(newArray.reverse().join("-"));
+//     event.preventDefault();
+//   });
+// });
 
-    var newArray = arrayOfStrings.filter(function(word){
-      return word.length >= 3;
+// Use a forEach() loop within another forEach() loop to build an array representing a deck of cards. A deck consists of 52 cards - 13 ranks in each of 4 suits.
+// Then, display a list of every card in the deck. (Hint: Each element of the array should read something like "ace of spades" or "4 of hearts").
+
+$(document).ready(function(){
+  var ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+  var suits = ["diamonds", "clubs", "spades", "hearts",]
+
+  suits.forEach(function(suit){
+    ranks.forEach(function(rank) {
+      $("ul").append("<li>" + rank + " of " + suit + "</li>");
     });
-    console.log(arrayOfStrings);
-    console.log(arrayOfStrings[1]);
-    console.log(newArray.reverse().join("-"));
-    event.preventDefault();
+    // $("ul").append("<li>" + rank + "</li>");
+    // ranks.forEach(function(suits){
+    //   $("ul").append(suits);
+    // });
   });
 });
